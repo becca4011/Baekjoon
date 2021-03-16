0a2bb148fa11e5e9c8aa8cc3600c6ac16ed8aaee
becca4011 <becca4011@naver.com>
2021-03-16 20:46:57 +0900

    boj

---
+++ 2231_분해합.py
@@ -0,0 +1,24 @@
+N = int(input())
+
+if len(str(N)) == 1 and N % 2 != 0:
+    print(0)
+else:
+    for i in range(1, N):
+        # 방법 1
+        nsum = list(map(int, list(str(i))))
+        lsum = i + sum(nsum)
+
+        """
+        # 방법 2
+        L = len(str(i))
+        lsum = i
+        for j in range(1, L+1):
+            lsum += i // 10 ** (L - j) % 10
+        """
+
+        if lsum == N:
+            print(i)
+            break
+
+        elif i == N - 1:
+            print(0)
