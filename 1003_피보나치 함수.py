33033881cb957184b6edf117035fea3d2a9c2dd3
becca4011 <becca4011@naver.com>
2021-04-13 18:12:08 +0900

    boj

--- 1003_피보나치 함수.py
+++ 1003_피보나치 함수.py
@@ -1,14 +1,25 @@
-"""
+import sys
+
 def fibo(n):
+    zero = [1, 0]
+    one = [0, 1]
+
     if n <= 1:
-        return n
-    else:
-        return(fibo(n - 1) + fibo(n - 2))
+        return
+    for x in range(2, n + 1):
+        zero.append(zero[x - 1] + zero[x - 2])
+        one.append(one[x - 1] + one[x - 2])
 
-for i in range(1, 10):
-    print(fibo(i))
+    return zero, one
+
+T = int(sys.stdin.readline())
+zero, one = fibo(40)
+
+for i in range(T):
+    N = int(sys.stdin.readline())
+    print(zero[N], one[N])
+
 """
-
 fibo = [0]
 for x in range(1, 10):
     if x < 2:
@@ -17,3 +28,4 @@ for x in range(1, 10):
         fibo.append(fibo[x - 2] + fibo[x - 1])
 
 print(fibo)
+"""
