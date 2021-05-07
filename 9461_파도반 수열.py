365aca461df9b8c9a08dd6c45dac6459275c719d
becca4011 <becca4011@naver.com>
2021-05-07 23:32:38 +0900

    boj

--- 9461_파도반 수열.py
+++ 9461_파도반 수열.py
@@ -1,10 +1,10 @@
 import sys
 
 T = int(sys.stdin.readline())
-dp = [1, 1, 1] + [0] * 97
+dp = [1, 1, 1] + [0] * 97 # list 생성 (100개)
 
 for i in range(3, len(dp)):
-    dp[i] = dp[i - 3] + dp[i - 2]
+    dp[i] = dp[i - 3] + dp[i - 2] # 점화식
 
 for i in range(T):
     N = int(sys.stdin.readline())
