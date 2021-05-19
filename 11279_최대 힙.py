aafbbd608b7d277f816dadcfdc25e15bf196410e
becca4011 <becca4011@naver.com>
2021-05-19 16:41:36 +0900

    boj

---
+++ 11279_최대 힙.py
@@ -0,0 +1,16 @@
+import sys
+from queue import PriorityQueue
+
+N = int(sys.stdin.readline())
+que = PriorityQueue()
+
+for i in range(N):
+    x = int(sys.stdin.readline())
+
+    if x == 0:
+        if que.empty():
+            print(0)
+        else:
+            print(-que.get())
+    else:
+        que.put(x * -1) # -1을 곱해서 순서를 바꿔주기
