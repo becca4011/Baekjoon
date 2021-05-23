be4e8bb2d985dfe9de09c899e46ea18bddedbdef
becca4011 <becca4011@naver.com>
2021-05-23 20:10:54 +0900

    boj

---
+++ 11286_절댓값 힙.py
@@ -0,0 +1,16 @@
+import sys
+from queue import PriorityQueue
+
+N = int(sys.stdin.readline())
+que = PriorityQueue()
+
+for i in range(N):
+    x = int(sys.stdin.readline())
+
+    if x == 0:
+        if que.empty():
+            print(0)
+        else:
+            print(que.get()[1])
+    else:
+        que.put((abs(x), x))
