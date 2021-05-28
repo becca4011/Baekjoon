77b42feb7f9c920c355359a4d0c60b2ac69a641a
becca4011 <becca4011@naver.com>
2021-05-28 12:03:17 +0900

    boj

--- 7662_이중 우선순위 큐.py
+++ 7662_이중 우선순위 큐.py
@@ -1,32 +1,42 @@
 # 미해결
 import sys
-from queue import PriorityQueue
+import heapq
+
+def pop(que, visited):
+    while que and not visited[que[0][1]]:
+        heapq.heappop(que)
 
 T = int(sys.stdin.readline())
 
 for i in range(T):
-    que_min = PriorityQueue()
-    que_max = PriorityQueue()
+    que_min = []
+    que_max = []
+    visited = [False] * 1000000
 
     k = int(sys.stdin.readline())
     for j in range(k):
         Q = sys.stdin.readline().split()
 
         if Q[0] == "I":
-            que_min.put(int(Q[1]))
-            que_max.put(-1 * int(Q[1]))
+            heapq.heappush(que_min, (int(Q[1]), j))
+            heapq.heappush(que_max, (-1 * int(Q[1]), j))
+            visited[j] = True
         elif Q[0] == "D":
-            if que_min.empty() and que_max.empty():
-                continue
-            if int(Q[1]) == -1:
-                que_min.get()
-            else:
-                que_max.get()
+            if Q[1] == "-1":
+                pop(que_min, visited)
+                if que_min:
+                    visited[que_min[0][1]] = False
+                    heapq.heappop(que_min)
+            elif Q[1] == "1":
+                pop(que_max, visited)
+                if que_max:
+                    visited[que_max[0][1]] = False
+                    heapq.heappop(que_max)
 
-    mx = -1 * min(que_max.queue)
-    mn = min(que_min.queue)
+    pop(que_max, visited)
+    pop(que_min, visited)
 
-    if mx == mn:
-        print("EMPTY")
+    if que_max:
+        print(-que_max[0][0], que_min[0][0])
     else:
-        print(mx, mn)
+        print("EMPTY")
